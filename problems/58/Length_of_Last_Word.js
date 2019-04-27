6680f358df588d822f8534461dab6908f5ea99bd
Damon-Salvatore <1150721387@qq.com>
2019-04-27 23:32:32 +0800

    add problem 58
    58

---
+++ problems/58/Length_of_Last_Word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  return s
+    .trim()
+    .split(' ')
+    .pop().length;
+};
+
+console.log(lengthOfLastWord('a '));
