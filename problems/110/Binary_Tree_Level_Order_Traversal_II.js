21add5c3cb47a985fd06fcbbf1ed7cb01e617121
Damon-Salvatore <1150721387@qq.com>
2019-05-19 19:35:37 +0800

    modify 110

--- problems/110/Binary_Tree_Level_Order_Traversal_II.js
+++ problems/110/Binary_Tree_Level_Order_Traversal_II.js
@@ -5,20 +5,44 @@
  *     this.left = this.right = null;
  * }
  */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
 /**
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrderBottom = function(root) {
-  var result = [];
-  var index = 0;
-  var recursive = function(node, index) {
-    if (!node) return;
-    if (index >= result.length) result[index] = [];
+
+let levelOrderBottom = function(root) {
+  let result = [];
+
+  let getArray = function(node, index) {
+    if (node == null) return [];
+    if (index >= result.length) {
+      result[index] = [];
+    }
     result[index].push(node.val);
-    recursive(node.left, index + 1);
-    recursive(node.right, index + 1);
+    getArray(node.left, index + 1);
+    getArray(node.right, index + 1);
   };
-  recursive(root, 0);
+  getArray(root, 0);
   return result.reverse();
 };
+
+let root = new TreeNode(3);
+let left1 = new TreeNode(9);
+let right1 = new TreeNode(20);
+
+let right1_1 = new TreeNode(15);
+let right1_2 = new TreeNode(7);
+
+right1.left = right1_1;
+right1.right = right1_2;
+
+root.left = left1;
+root.right = right1;
+
+let answer = levelOrderBottom(root);
+
+console.log(answer);
