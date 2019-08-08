8b6068ab85ff08584d8245a093b716e99b288607
Damon-Salvatore <1150721387@qq.com>
2019-08-08 13:59:56 +0800

    Update Pascal's_Triangle.js

--- problems/118/Pascal's_Triangle.js
+++ problems/118/Pascal's_Triangle.js
@@ -3,5 +3,27 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    
+  let result = [];
+  if (numRows < 1) return result;
+  result[0] = [];
+  result[0].push(1);
+  if (numRows == 1) return result;
+
+  result[1] = [];
+  result[1].push(1, 1);
+  if (numRows == 2) return result;
+
+  for (let i = 2; i < numRows; i++) {
+    result[i] = [];
+    result[i].push(1);
+    for (let j = 0; j < result[i - 1].length - 1; j++) {
+      result[i].push(result[i - 1][j] + result[i - 1][j + 1]);
+    }
+    result[i].push(1);
+  }
+
+  return result;
 };
+
+let result = generate(5);
+console.log(result);
